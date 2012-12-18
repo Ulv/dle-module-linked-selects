be75d05602083668001491055c5c1767128e3b69
Vladimir Chmil <ulv8888@gmail.com>
2012-12-18 12:12:32 +0200

    js и php готовы полностью

--- engine/ajax/education.php
+++ engine/ajax/education.php
@@ -11,9 +11,9 @@
 header('Content-type: application/json');
 include ('../api/api.class.php');
 
-if (isset($_GET["specid"]) || isset($_GET["regid"]) || isset($_GET["vuzid"])) 
+if (isset($_GET["specid"]) || isset($_GET["regid"]) || isset($_GET["vuzid"]))
 {
-    if (isset($_GET["specid"])) {
+    if (isset($_GET["specid"]) && !isset($_GET["regid"])) {
         // выбираем регионы по коду специальности
         $spec_id = preg_replace("#[^0-9]#","", substr($_GET["specid"], 0, 4));
         $sql = $db->query("SELECT distinct region.id as id, region.title as title FROM region, spectovuz WHERE region.id = spectovuz.region_id and spectovuz.spec_id=".$spec_id." order by region.title asc");
@@ -23,12 +23,34 @@ if (isset($_GET["specid"]) || isset($_GET["regid"]) || isset($_GET["vuzid"]))
             $result[] = array("id"=>$row["id"], "title"=>$row["title"]);
         }
 
-
         if (!empty($result)) {
             echo json_encode(array('status'=>'ok', 'data'=>$result));
         } else {
             echo json_encode(array('status'=>'err', 'data'=>null));
         }
     }
+
+    if (isset($_GET["specid"]) && isset($_GET["regid"])) {
+        // выбираем вузы по региону и специальности
+        $spec_id = preg_replace("#[^0-9]#","", substr($_GET["specid"], 0, 4));
+        $reg_id  = preg_replace("#[^0-9]#","", substr($_GET["regid"], 0, 4));
+
+        $sql = $db->query("SELECT distinct vuz.id as id, vuz.title as title FROM vuz, spectovuz WHERE vuz.id = spectovuz.vuz_id and spectovuz.spec_id=".$spec_id." and spectovuz.region_id=".$reg_id." order by vuz.title asc");
+
+        $result = array();
+        while ($row = $db->get_row($sql)) {
+            $result[] = array("id"=>$row["id"], "title"=>$row["title"]);
+        }
+
+        if (!empty($result)) {
+            echo json_encode(array('status'=>'ok', 'data'=>$result));
+        } else {
+            echo json_encode(array('status'=>'err', 'data'=>null));
+        }
+
+    }
 }
+    /*else {
+    echo "Incorrect parameters"
+}*/
 ?>
--- engine/modules/education.php
+++ engine/modules/education.php
@@ -33,11 +33,16 @@
                 list_vuz    = $("#education_vuz"),
                 specid;
 
+            // меняем специальность - заполняем регион
             list_spec.change(function(){
+                list_vuz.empty();
+                list_vuz.append('<option value="0">--- выберите специальность и регион ---</option>');
+
                 specid = $(this).val();
                 chSelect(list_region, 'specid=' + specid);
             });
 
+            // меняем регион - заполняем вузы
             list_region.change(function(){
                 if (undefined !== specid) {
                     chSelect(list_vuz, 'specid=' + specid + '&regid=' + $(this).val());
@@ -78,7 +83,7 @@ include ('engine/api/api.class.php');
 ?>
 
 <label for="education_spec"><?=$lang["education_spec"]; ?></label>
-<select class="education" id="education_spec">
+<select class="education" id="education_spec" name="education_spec">
 <?php $sql = $db->query("select * from spec"); while ($row = $db->get_row($sql)): ?>
     <option value="<?=$row['id'];?>"><?=$row['title'];?></option>
 <?php endwhile; ?>
@@ -86,13 +91,13 @@ include ('engine/api/api.class.php');
 
 <br />
 <label for="education_region"><?=$lang["education_region"];?></label>
-<select class="education" id="education_region">
+<select class="education" id="education_region" name="education_region">
     <option value="0">--- выберите специальность ---</option>
 </select>
 
 <br />
 <label for="education_vuz"><?=$lang["education_vuz"];?></label>
-<select class="education" id="education_vuz">
+<select class="education" id="education_vuz" name="education_vuz">
     <option value="0">--- выберите специальность и регион ---</option>
 </select>
 
